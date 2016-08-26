45f615608e742bb9aecb03d9bb74e2615fe93ace
shengfeng.lai <laishengfeng@nmtx.com>
2016-08-26 11:49:27 +0800

    add StringUtils.right() and StringUtils.left() test case

--- src/main/java/com/lsf/commons/lang3/StringUtilsTest.java
+++ src/main/java/com/lsf/commons/lang3/StringUtilsTest.java
@@ -1,14 +1,26 @@
 package com.lsf.commons.lang3;
 
 import org.apache.commons.lang3.StringUtils;
+import org.joda.time.DateTimeUtils;
 
 public class StringUtilsTest {
 
     public static void main(String[] args) {
-        //judge null and " " 
+
         System.out.println(StringUtils.isEmpty(""));
         System.out.println(StringUtils.isEmpty(null));
         System.out.println(StringUtils.isNotEmpty(null));
-        
+
+        System.out.println(testStringUtilsRight());
+        System.out.println(testStringUtilsLeft());
+
+    }
+
+    public static String testStringUtilsRight() {
+        return StringUtils.right(String.valueOf(DateTimeUtils.currentTimeMillis() / 1000), 6);
+    }
+
+    public static String testStringUtilsLeft() {
+        return StringUtils.left(String.valueOf(DateTimeUtils.currentTimeMillis() / 1000), 6);
     }
 }
