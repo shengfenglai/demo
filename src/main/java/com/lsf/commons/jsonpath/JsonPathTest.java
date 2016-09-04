e48563a902a1c4b57fedf6a3d1475b42b8535746
shengfeng.lai <laishengfeng@nmtx.com>
2016-09-04 21:53:40 +0800

    fix

--- src/main/java/com/lsf/commons/jsonpath/JsonPathTest.java
+++ src/main/java/com/lsf/commons/jsonpath/JsonPathTest.java
@@ -16,7 +16,7 @@ public class JsonPathTest {
 
         System.out.println();
 
-        // 输出全部author的值
+
         List<String> authors = JsonPath.read(json, "$.store.book[*].author");
         for (String auth : authors) {
             System.out.println(auth);
@@ -24,7 +24,7 @@ public class JsonPathTest {
 
         System.out.println();
 
-        // 输出book[*]中category == 'reference'的book
+
         List<Object> books = JsonPath.read(json, "$.store.book[?(@.category == 'reference')]");
         for (Object o : books) {
             System.out.println(o);
@@ -32,7 +32,7 @@ public class JsonPathTest {
 
         System.out.println();
 
-        // 输出book[*]中price>10的book
+
         List<Object> books2 = JsonPath.read(json, "$.store.book[?(@.price>10)]");
         for (Object o : books2) {
             System.out.println(o);
@@ -40,7 +40,7 @@ public class JsonPathTest {
 
         System.out.println();
 
-        // 输出book[*]中含有isbn元素的book
+
         List<Object> books3 = JsonPath.read(json, "$.store.book[?(@.isbn)]");
         for (Object o : books3) {
             System.out.println(o);
@@ -48,7 +48,7 @@ public class JsonPathTest {
 
         System.out.println();
 
-        // 输出该json中所有price的值
+
         List<Double> prices = JsonPath.read(json, "$..price");
         for (Double price : prices) {
             System.out.println(price);
