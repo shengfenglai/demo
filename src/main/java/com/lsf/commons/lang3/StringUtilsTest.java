b347b2a1c1e00ad3859f2f0636f1527ae5d76432
shengfeng.lai <laishengfeng@nmtx.com>
2016-09-05 09:47:10 +0800

    chop and isNotBlank

--- src/main/java/com/lsf/commons/lang3/StringUtilsTest.java
+++ src/main/java/com/lsf/commons/lang3/StringUtilsTest.java
@@ -14,6 +14,10 @@ public class StringUtilsTest {
         System.out.println(testStringUtilsRight());
         System.out.println(testStringUtilsLeft());
 
+        System.out.println(testStringUtilsIsNotBlank("hello"));
+
+        System.out.println(testStringUtilsChop("hello world"));
+
     }
 
     public static String testStringUtilsRight() {
@@ -23,4 +27,13 @@ public class StringUtilsTest {
     public static String testStringUtilsLeft() {
         return StringUtils.left(String.valueOf(DateTimeUtils.currentTimeMillis() / 1000), 6);
     }
+
+    public static boolean testStringUtilsIsNotBlank(String str) {
+        return StringUtils.isNotBlank(str);
+    }
+
+    public static String testStringUtilsChop(String str) {
+        return StringUtils.chop(str);
+    }
+
 }
