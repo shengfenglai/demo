f9f3349fd072bf3a5b7a9e4ebf0554d0e304dc40
shengfeng.lai <laishengfeng@nmtx.com>
2016-11-28 15:17:19 +0800

    just play

---
+++ src/main/java/com/lsf/String/StringTest.java
@@ -0,0 +1,16 @@
+package com.lsf.String;
+
+/**
+ * Created by shengfeng on 2016/11/28.
+ */
+public class StringTest {
+
+    public static void main(String[] args) {
+        String s1 = "helloworld";
+        String s2 = new String("helloworld");
+        String s3 = "hello" + "world";
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s3);
+        System.out.println(s1 == s1.intern());
+    }
+}
