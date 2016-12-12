bf0a2cd2984bfd4654ea83e62b1fdf83f362cf17
shengfeng.lai <laishengfeng@nmtx.com>
2016-12-12 15:00:58 +0800

    add reflectTest

---
+++ src/main/java/com/lsf/object/ReflectTest.java
@@ -0,0 +1,17 @@
+package com.lsf.object;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Created by shengfeng on 2016/12/12.
+ */
+public class ReflectTest {
+
+    public static void main(String[] args) throws Exception {
+        String str = "hello";
+
+        Method m = str.getClass().getMethod("toUpperCase");
+        System.out.println(m.invoke(str));
+    }
+}
