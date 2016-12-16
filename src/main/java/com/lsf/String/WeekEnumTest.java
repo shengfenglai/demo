b75e9edb1301fd5745e87e9e67e2b378d56cc398
shengfeng.lai <laishengfeng@nmtx.com>
2016-12-16 16:33:11 +0800

    add WeekEnumTest

---
+++ src/main/java/com/lsf/String/WeekEnumTest.java
@@ -0,0 +1,32 @@
+package com.lsf.String;
+
+/**
+ * Created by shengfeng on 2016/12/16.
+ */
+public enum WeekEnumTest {
+    MONDAY(1),
+    TUESDAY(2),
+    WEDNESDAY(3),
+    THURSDAY(4),
+    FRIDAY(5),
+    SATURDAY(6),
+    SUNDAY(7);
+
+    private int key;
+
+    private WeekEnumTest(int key) {
+        this.key = key;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(WeekEnumTest.FRIDAY.getKey());
+    }
+}
